97a7df80e084e2b0be2f9d8d5bea20706aab8473
Ding <38353554+nuoyi-618@users.noreply.github.com>
2019-12-15 16:45:52 +0800

    Add files via upload

---
+++ cspider/hash_functions.c
@@ -0,0 +1,274 @@
+/*
+ **************************************************************************
+ *                                                                        *
+ *          General Purpose Hash Function Algorithms Library              *
+ *                                                                        *
+ * Author: Arash Partow - 2002                                            *
+ * URL: http://www.partow.net                                             *
+ * URL: http://www.partow.net/programming/hashfunctions/index.html        *
+ *                                                                        *
+ * Copyright notice:                                                      *
+ * Free use of the General Purpose Hash Function Algorithms Library is    *
+ * permitted under the guidelines and in accordance with the MIT License. *
+ * http://www.opensource.org/licenses/MIT                                 *
+ *                                                                        *
+ **************************************************************************
+*/
+
+
+#include "cspider.h"
+
+
+unsigned int RSHash(char* str, unsigned int len)
+{
+	unsigned int b = 378551;
+	unsigned int a = 63689;
+	unsigned int hash = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = hash * a + (*str);
+		a = a * b;
+	}
+
+	return hash;
+}
+/* End Of RS Hash Function */
+
+
+unsigned int JSHash(char* str, unsigned int len)
+{
+	unsigned int hash = 1315423911;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash ^= ((hash << 5) + (*str) + (hash >> 2));
+	}
+
+	return hash;
+}
+/* End Of JS Hash Function */
+
+
+unsigned int PJWHash(char* str, unsigned int len)
+{
+	const unsigned int BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
+	const unsigned int ThreeQuarters = (unsigned int)((BitsInUnsignedInt * 3) / 4);
+	const unsigned int OneEighth = (unsigned int)(BitsInUnsignedInt / 8);
+	const unsigned int HighBits = (unsigned int)(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+	unsigned int hash = 0;
+	unsigned int test = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = (hash << OneEighth) + (*str);
+
+		if ((test = hash & HighBits) != 0)
+		{
+			hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+		}
+	}
+
+	return hash;
+}
+/* End Of  P. J. Weinberger Hash Function */
+
+
+unsigned int ELFHash(char* str, unsigned int len)
+{
+	unsigned int hash = 0;
+	unsigned int x = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = (hash << 4) + (*str);
+		if ((x = hash & 0xF0000000L) != 0)
+		{
+			hash ^= (x >> 24);
+		}
+		hash &= ~x;
+	}
+
+	return hash;
+}
+/* End Of ELF Hash Function */
+
+
+unsigned int BKDRHash(char* str, unsigned int len)
+{
+	unsigned int seed = 131; /* 31 131 1313 13131 131313 etc.. */
+	unsigned int hash = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = (hash * seed) + (*str);
+	}
+
+	return hash;
+}
+/* End Of BKDR Hash Function */
+
+
+unsigned int SDBMHash(char* str, unsigned int len)
+{
+	unsigned int hash = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = (*str) + (hash << 6) + (hash << 16) - hash;
+	}
+
+	return hash;
+}
+/* End Of SDBM Hash Function */
+
+
+unsigned int DJBHash(char* str, unsigned int len)
+{
+	unsigned int hash = 5381;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = ((hash << 5) + hash) + (*str);
+	}
+
+	return hash;
+}
+/* End Of DJB Hash Function */
+
+
+unsigned int DEKHash(char* str, unsigned int len)
+{
+	unsigned int hash = len;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = ((hash << 5) ^ (hash >> 27)) ^ (*str);
+	}
+	return hash;
+}
+/* End Of DEK Hash Function */
+
+
+unsigned int BPHash(char* str, unsigned int len)
+{
+	unsigned int hash = 0;
+	unsigned int i = 0;
+	for (i = 0; i < len; str++, i++)
+	{
+		hash = hash << 7 ^ (*str);
+	}
+
+	return hash;
+}
+/* End Of BP Hash Function */
+
+
+unsigned int FNVHash(char* str, unsigned int len)
+{
+	const unsigned int fnv_prime = 0x811C9DC5;
+	unsigned int hash = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash *= fnv_prime;
+		hash ^= (*str);
+	}
+
+	return hash;
+}
+/* End Of FNV Hash Function */
+
+
+unsigned int APHash(char* str, unsigned int len)
+{
+	unsigned int hash = 0xAAAAAAAA;
+	unsigned int i = 0;
+
+	for (i = 0; i < len; str++, i++)
+	{
+		hash ^= ((i & 1) == 0) ? ((hash << 7) ^ (*str) * (hash >> 3)) :
+			(~((hash << 11) + ((*str) ^ (hash >> 5))));
+	}
+
+	return hash;
+}
+/* End Of AP Hash Function */
+
+
+unsigned int MyHash(char* str, unsigned int len)
+{
+	unsigned int hash = 0;
+	char* ptr = str;
+
+	int param = 1;
+	while ((*ptr) != '\0') {
+		hash += (param * (*ptr)) % len;
+		param = (param << 2) % len;
+		++ptr;
+	}
+
+	return (hash * 999983) % len;
+}
+/* End Of My Hash Function */
+
+//超级哈希from google，参数seed确定具体哈希函数  hashResult = MurmurHash2(line, strlen(line), i + 1)
+unsigned int MurmurHash2(const void * key, int len, unsigned int seed)
+{
+	// 'm' and 'r' are mixing constants generated offline.
+	// They're not really 'magic', they just happen to work well.
+
+	const unsigned int m = 0x5bd1e995; // 0101 1011 1101 0001 1110 1001 1001 0101
+	const int r = 24;
+
+	// Initialize the hash to a 'random' value
+
+	unsigned int h = seed ^ len;  //二进制按位异或
+
+								  // Mix 4 bytes at a time into the hash
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	while (len >= 4)
+	{
+		unsigned int k = *(unsigned int *)data;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+
+	// Handle the last few bytes of the input array
+
+	switch (len)
+	{
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0];
+		h *= m;
+	}
+
+	// Do a few final mixes of the hash to ensure the last few
+	// bytes are well-incorporated.
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
+}
